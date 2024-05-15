ba93fdff27281ef7bcc305af013742b548b3d0d4
Xiansheng Cai <iintsjds@gmail.com>
2024-05-15 20:23:29 +0800

    wip

--- legacy/example/berkeleygw/berkeleygw_io.jl
+++ legacy/example/berkeleygw/berkeleygw_io.jl
@@ -35,19 +35,74 @@ end
 function load_cell(wfn)
     # load cell info from wfn.h5
     @unpack celvol, recvol, alat, blat, nat, avec, bvec, adot, bdot, atyp, apos = wfn["mf_header/crystal"]
-    println(alat * blat)
-    println(celvol * recvol)
-    println(avec)
-    # cell = Cells.Cell(;lattice=)
+    lattice = alat .* avec
+    # currently only for simple case
+    # atoms = Vector{Int64}(atyp)
+    atoms = Vector{Int64}([i for i in 1:length(atyp)])
+    positions = [apos[:, i] for i in 1:nat]
+    cell = Cells.Cell(; lattice=Matrix(lattice), atoms=atoms, positions=positions)
+    return cell
+end
+
+function compute_meshmap(bzmesh, rk, mtrx)
+    nrk = size(rk)[2]
+    irreducible_indices = zeros(Int, nrk)
+    for i in 1:nrk
+        k = rk[:, i]
+        irreducible_indices[i] = AbstractMeshes.locate(bzmesh, AbstractMeshes.frac_to_cart(bzmesh, k))
+        @assert bzmesh[AbstractMeshes.FracCoords, irreducible_indices[i]] ≈ k
+    end
+    # constructing map
+    map = zeros(Int, length(bzmesh))
+    for i in 1:length(bzmesh)
+        k_frac = Vector(bzmesh[AbstractMeshes.FracCoords, i])
+        for m in 1:3
+            if k_frac[m] >= 0.5
+                k_frac[m] -= 1
+            end
+        end
+        k = AbstractMeshes.frac_to_cart(bzmesh, k_frac)
+        for j in 1:size(mtrx)[3]
+            symat = mtrx[:, :, j]
+            newk = symat * k
+            newk_frac = AbstractMeshes.cart_to_frac(bzmesh, newk)
+            for m in 1:3
+                if newk_frac[m] >= 0.5
+                    newk_frac[m] -= 1
+                elseif newk_frac[m] < -0.5
+                    newk_frac[m] += 1
+                end
+            end
+            newk_frac .+= 0.5
+            newk = AbstractMeshes.frac_to_cart(bzmesh, newk_frac)
+            for m in 1:nrk
+                frac_rk = rk[:, m]
+                cart_rk = AbstractMeshes.frac_to_cart(bzmesh, frac_rk)
+                if isapprox(cart_rk, newk, atol=1e-6)
+                    # println("k=$k, mat=$(symat), newk=$newk, rk=$(cart_rk)")
+                    println("k=$k_frac, mat=$(symat), newk=$newk_frac, rk=$(frac_rk)")
+                    # @assert (map[i] == 0 || map[i] == m) "$k reduced to two points:$(map[i]) and $m !"
+                    map[i] = m
+                end
+            end
+        end
+        # @assert map[i] != 0 "found no rk for i=$i, k=$k !"
+    end
+    return
 end
 
 function load_rbzmesh(wfn)
     # load cell
-    bgw_lattice = wfn[""]
+    cell = load_cell(wfn)
+    @unpack ntran, cell_symmetry, mtrx, tnp = wfn["mf_header/symmetry"]
+    @unpack nspin, nspinor, nrk, mnband, ngkmax, ecutwfc, kgrid, shift, ngk, ifmin, ifmax, w, rk, el, occ, = wfn["mf_header/kpoints"]
 
-    bgw_kgrid = wfn["mf_header/kpoints/kgrid"]
-    bgw_shift = wfn["mf_header/kpoints/shift"]
+    isshift = [(s == 0.5) ? true : false for s in shift] # only apply for 0 and 1/2 shift
+    # BGW set origin at 0
+    bzmesh = BZMeshes.UniformBZMesh(cell=cell, origin=0.0, size=kgrid, shift=isshift)
 
+    # construct symmetry map
+    meshmap = compute_meshmap(bzmesh, rk, mtrx)
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
--- legacy/example/berkeleygw/test_bgw_io.jl
+++ legacy/example/berkeleygw/test_bgw_io.jl
@@ -1,20 +1,40 @@
 
 include("berkeleygw_io.jl")
 
-using Test
+using Test, LinearAlgebra
 
-@testset "Cell" begin
-
+@testset "Wave function" begin
     wfn = jldopen("wfn.h5", "r")
 
-    @testset "Test BerkeleyGW conventions" begin
+    @testset "Cell" begin
+
         @unpack celvol, recvol, alat, blat, nat, avec, bvec, adot, bdot, atyp, apos = wfn["mf_header/crystal"]
-        @test (alat * blat) ≈ 2π
-        @test (celvol * recvol) ≈ (2π)^3
-        println(alat)
+
+        @testset "Test BerkeleyGW conventions" begin
+            @test celvol ≈ det(alat .* avec)
+            @test recvol ≈ det(blat .* bvec)
+            @test (alat * blat) ≈ 2π
+            @test (celvol * recvol) ≈ (2π)^3
+
+            @test nat == length(atyp) == size(apos)[2]
+        end
+
+        @testset "Test loaded cell" begin
+            cell = load_cell(wfn)
+            println(cell)
+            @test cell.cell_volume ≈ celvol
+            @test cell.recip_cell_volume ≈ recvol
+            @test cell.lattice ≈ (alat .* avec)
+            @test cell.recip_lattice ≈ (blat .* bvec)
+        end
+
     end
 
-    # wfn = jldopen("wfn.h5", "r")
-    # cell = load_cell(wfn)
+    @testset "ReducedBZMesh" begin
+        @unpack nspin, nspinor, nrk, mnband, ngkmax, ecutwfc, kgrid, shift, ngk, ifmin, ifmax, w, rk, el, occ, = wfn["mf_header/kpoints"]
+
+        rbzmesh = load_rbzmesh(wfn)
+
+    end
 
 end
